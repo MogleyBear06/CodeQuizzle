99f493ff0e5a486b5cd27d803695346d889ec2ca
MogleyBear06 <nicholas.james.ortiz@gmail.com>
2023-07-23 00:18:24 -0500

    added js

---
+++ script.js
@@ -0,0 +1,14 @@
+//  Quiz timer
+var  quizTime = 60;
+
+//
+var timeInterval // make this global 
+// player score
+var playerScore = 0;
+
+var Highscore = 0
+
+var scoreList =[]
+
+// variable to tracker current question
+currentQuestion = 0
